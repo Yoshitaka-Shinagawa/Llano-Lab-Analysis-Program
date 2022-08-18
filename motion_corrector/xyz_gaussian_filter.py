e0194f1cda0f6f326f488cedbbefbc89f5606278
Yoshitaka Shinagawa <60482573+Yoshitaka-Shinagawa@users.noreply.github.com>
2022-08-18 16:46:37 -0500

    Made lines adhere to 80 char limit

--- motion_corrector/xyz_gaussian_filter.py
+++ motion_corrector/xyz_gaussian_filter.py
@@ -32,7 +32,8 @@ def xyz_gaussian_filter(folder_images,x_sigma,y_sigma,z_sigma):
     """
     
     # Goes through each image and applies a 2D Gaussian blur
-    filtered_images = Parallel(n_jobs=12)(delayed(gaussian_filter)(image,sigma=(x_sigma,y_sigma),mode="nearest") for image in folder_images)
+    filtered_images = Parallel(n_jobs=12)(delayed(gaussian_filter)(image,
+        sigma=(x_sigma,y_sigma),mode="nearest") for image in folder_images)
     filtered_images = np.array(filtered_images)
     
     # Loop constants
@@ -41,7 +42,9 @@ def xyz_gaussian_filter(folder_images,x_sigma,y_sigma,z_sigma):
     
     # Goes through each pixel in z direction
     for row in range(rows):
-        columns_data = Parallel(n_jobs=12)(delayed(gaussian_filter1d)(filtered_images[:,row,column],sigma=z_sigma,mode="nearest") for column in range(columns))
+        columns_data = Parallel(n_jobs=12)(delayed(gaussian_filter1d)(
+            filtered_images[:,row,column],sigma=z_sigma,mode="nearest") 
+            for column in range(columns))
         for column in range(columns):
             filtered_images[:,row,column] = columns_data[column]
     
