748a663c891468af9698ae97d4ac568cc1332a27
Yoshitaka Shinagawa <60482573+Yoshitaka-Shinagawa@users.noreply.github.com>
2022-08-19 00:46:46 -0500

    Made lines adhere to 80 char limit

--- data_extractor_subtraction/data_reader.py
+++ data_extractor_subtraction/data_reader.py
@@ -44,18 +44,22 @@ def data_reader(image_stack,cell_arrays,background_arrays):
     # Goes through each image
     for image_number,image in enumerate(image_stack):
         
-        # Goes through each cell, gathers data from the image, and adds it to the array
+        # Goes through each cell, gathers data from the image, and adds it to 
+        # the array
         for cell_number in range(cell_total):
             
             # Finds average value of data and adds it to array
             if np.count_nonzero(cell_arrays[cell_number]) == 0:
                 data_value = 0
             else:
-                data_value = np.sum(image*cell_arrays[cell_number]) / np.count_nonzero(cell_arrays[cell_number])
+                data_value = np.sum(image*cell_arrays[cell_number]) / \
+                    np.count_nonzero(cell_arrays[cell_number])
             if np.count_nonzero(background_arrays[cell_number])!= 0:
-                background_value = np.sum(image*background_arrays[cell_number]) / np.count_nonzero(background_arrays[cell_number])
+                background_value = np.sum(image*background_arrays[cell_number]\
+                    ) / np.count_nonzero(background_arrays[cell_number])
             else:
                 background_value = 0
-            folder_data[cell_number,image_number] = data_value - 0.4*background_value
+            folder_data[cell_number,image_number] = data_value - 0.4 * \
+                background_value
     
     return folder_data
